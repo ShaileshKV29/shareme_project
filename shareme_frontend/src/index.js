a61fef91c603d02f1cd5f1ceb97c49618e50d025
ShaileshKV29 <shailesh29062001@gmail.com>
2023-07-14 16:30:52 +0530

    Changes And Login System

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {createRoot} from 'react-dom/client'
 import ReactDOM from "react-dom";
 
 import {BrowserRouter as Router} from 'react-router-dom'
@@ -6,4 +7,6 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"))
+
+root.render(<Router><App /></Router>);
